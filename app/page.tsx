010d23d2d0707da38403091ef2fa01ecb880821e
WasayBaig10 <wasaybaig617@gmail.com>
2024-12-09 00:43:25 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Main from '@/components/Main'
+import OurMenu from '@/components/OurMenu'
+import OurChefs from '@/components/OurChefs'
+import Checkout from'@/components/Checkout'
+function page() {
+ return (
+    <div>
+      <Main/>
+      {/* <OurMenu/> */}
+      {/* <OurChefs/> */}
+      {/* <Checkout/> */}
+    </div>
+  )
+}
+
+export default page
---
+++ components/Items.tsx
@@ -0,0 +1,138 @@
+import react from 'react';
+
+function Cart(){
+    <div>
+         <nav className="h-[60px] ml-auto mr-auto w-[80vw] flex items-center justify-between bg-black">
+        <div className="h-[60px] ml-auto mr-auto w-[80vw] flex items-center justify-between">
+          <div className="flex text-2xl">
+            <h1>Food</h1>
+            <h1 className="text">talk</h1>
+          </div>
+          <ul className="flex space-x-4">
+            <li className="text2">Home</li>
+            <li className="text2">Menu</li>
+            <li className="text2">Blog</li>
+            <li className="text2">Pages</li>
+            <li className="text2">About</li>
+            <li className="text2">Shop</li>
+            <li className="text2">Contact</li>
+          </ul>
+          <ul className="flex space-x-4">
+            <li>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.875 18.75C15.2242 18.75 18.75 15.2242 18.75 10.875C18.75 6.52576 15.2242 3 10.875 3C6.52576 3 3 6.52576 3 10.875C3 15.2242 6.52576 18.75 10.875 18.75Z"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M16.4431 16.4438L20.9994 21.0002"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </li>
+            <li>
+              <svg
+                width="28"
+                height="29"
+                viewBox="0 0 28 29"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14 15C17.3137 15 20 12.3137 20 9C20 5.68629 17.3137 3 14 3C10.6863 3 8 5.68629 8 9C8 12.3137 10.6863 15 14 15Z"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-miterlimit="10"
+                />
+                <g filter="url(#filter0_d_2003_823)">
+                  <path
+                    d="M4.90527 20.2491C5.82736 18.6531 7.15322 17.3278 8.74966 16.4064C10.3461 15.485 12.1569 15 14.0002 15C15.8434 15 17.6542 15.4851 19.2506 16.4065C20.8471 17.3279 22.1729 18.6533 23.0949 20.2493"
+                    stroke="white"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    shape-rendering="crispEdges"
+                  />
+                </g>
+                <defs>
+                  <filter
+                    id="filter0_d_2003_823"
+                    x="0.155029"
+                    y="14.25"
+                    width="27.6899"
+                    height="14.7495"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                  >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                      in="SourceAlpha"
+                      type="matrix"
+                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                      result="hardAlpha"
+                    />
+                    <feOffset dy="4" />
+                    <feGaussianBlur stdDeviation="2" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix
+                      type="matrix"
+                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in2="BackgroundImageFix"
+                      result="effect1_dropShadow_2003_823"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="effect1_dropShadow_2003_823"
+                      result="shape"
+                    />
+                  </filter>
+                </defs>
+              </svg>
+            </li>
+            <li>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19.5787 20.25H4.42122C4.23665 20.25 4.05856 20.1819 3.92103 20.0588C3.7835 19.9358 3.69619 19.7663 3.67581 19.5828L2.34248 7.58282C2.33083 7.47799 2.34143 7.37188 2.37357 7.27142C2.40572 7.17095 2.4587 7.0784 2.52904 6.99981C2.59939 6.92121 2.68553 6.85834 2.78182 6.81529C2.87812 6.77225 2.98241 6.75 3.08789 6.75H20.912C21.0175 6.75 21.1218 6.77225 21.2181 6.81529C21.3144 6.85834 21.4005 6.92121 21.4708 6.99981C21.5412 7.0784 21.5942 7.17095 21.6263 7.27142C21.6585 7.37188 21.6691 7.47799 21.6574 7.58282L20.3241 19.5828C20.3037 19.7663 20.2164 19.9358 20.0789 20.0588C19.9413 20.1819 19.7632 20.25 19.5787 20.25Z"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M8.25 9.75V6.75C8.25 5.75544 8.64509 4.80161 9.34835 4.09835C10.0516 3.39509 11.0054 3 12 3C12.9946 3 13.9484 3.39509 14.6517 4.09835C15.3549 4.80161 15.75 5.75544 15.75 6.75V9.75"
+                  stroke="white"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+}
+
+export default Cart;
